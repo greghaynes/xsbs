844d9a99ca04dd8f1cf89c1cbb514849d3570298
Gregory Haynes <greg@greghaynes.net>
2009-08-31 01:19:00 -0700

    Added template to geoip

--- src/pyscripts/geoip/geoip.py
+++ src/pyscripts/geoip/geoip.py
@@ -1,11 +1,23 @@
 import sbevents, sbserver, sbtools, pygeoip
+from ConfigParser import ConfigParser
+import string
+
 db = pygeoip.Database('geoip/GeoIP.dat')
+conf = ConfigParser()
+conf.read('geoip/plugin.conf')
+template = ''
+if conf.has_option('Config', 'template'):
+	template = string.Template(conf.get('Config', 'template'))
+else:
+	template = string.Template('$user has connected from $country.')
+del conf
 
 def getCountry(ip): 
 	 return db.lookup(pygeoip.num_to_addr(ip)).country
 
 def announce(cn):
-	sbserver.message(sbtools.orange(sbserver.playerName(cn) + " is connected from " + str(getCountry(sbserver.playerIpLong(cn)))))
+	msg = template.substitute(user=sbserver.playerName(cn), country=getCountry(sbserver.playerIpLong(cn)))
+	sbserver.message(msg)
 
 def init():
 	sbevents.registerEventHandler("player_active", announce)
