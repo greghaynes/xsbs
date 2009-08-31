07c5ebc53cdd43a83fc7ec2c587bffe490f030c1
Gregory Haynes <greg@greghaynes.net>
2009-08-31 03:06:12 -0700

    Added player_command event, added Bans plugin

---
+++ src/pyscripts/Bans/bans.py
@@ -0,0 +1,67 @@
+import sbserver, sbevents
+import time, string
+from ConfigParser import ConfigParser
+
+banned_ips = {}
+ban_command = 'ban'
+default_ban_length = 3600
+ban_message = string.Template('Banning $name for $seconds seconds for $reason.')
+default_reason = 'unspecified reason'
+
+def onMessage(cn, text):
+	sp = text.split(' ')
+	if sp[0] == ban_command and sp[1]:
+		tcn = int(sp[1])
+		ip = sbserver.playerIpLong(tcn)
+		if not ip:
+			sbserver.message(cn, 'Invalid cn')
+			return
+		reason = ''
+		length = 0
+		if len(sp) >= 4:
+			reason = sp[3]
+			length = int(sp[2])
+		elif len(sp) == 3:
+			reason = default_reason
+			length = int(sp[2])
+		else:
+			return
+		ban(cn, ip, length, reason)
+		sbserver.playerKick(tcn)
+
+def ban(cn, ip, seconds, reason):
+	msg = ban_message.substitute(name=sbserver.playerName(cn), seconds=seconds, reason=reason)
+	sbserver.message(msg)
+	expiration = time.time() + seconds
+	if banned_ips.has_key(ip):
+		if banned_ips[ip] > expiration:
+			return
+	banned_ips[ip] = expiration
+
+def allowClient(cn):
+	ip = sbserver.playerIpLong(cn)
+	if banned_ips.has_key(ip):
+		expiration = banned_ips[ip]
+		if time.time() >= expiration:
+			banned_ips.pop(ip)
+			return True
+		return False
+	return True
+
+def init():
+	sbevents.registerPolicyEventHandler("allow_connect", allowClient)
+	sbevents.registerEventHandler("player_command", onMessage)
+
+config = ConfigParser()
+config.read('Bans/plugin.conf')
+if config.has_option('Config', 'ban_command'):
+	ban_command = config.get('Config', 'ban_command')
+if config.has_option('Config', 'default_ban_length'):
+	default_ban_length = config.get('Config', 'default_ban_length')
+if config.has_option('Config', 'ban_message'):
+	ban_message = config.get('Config', 'ban_message')
+if config.has_option('Config', 'default_reason'):
+	default_reason = config.get('Config', 'default_reason')
+del config
+
+init()
--- src/pyscripts/sbtools.py
+++ src/pyscripts/sbtools.py
@@ -33,3 +33,9 @@ def colorstring(str, text):
 		return orange(text)
 	if str == 'white':
 		return white(text)
+
+def ipLongToString(num):
+	return '%d.%d.%d.%d' % ((num & 0xff),
+		(num >> 8) & 0xff,
+		(num >> 16) & 0xff,
+		(num >> 24) & 0xff)
