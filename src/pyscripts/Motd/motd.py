7d4241463846807f18471d23b42e261f258d4552
Gregory Haynes <greg@greghaynes.net>
2009-08-30 17:17:35 -0700

    Added Motd plugin

---
+++ src/pyscripts/Motd/motd.py
@@ -0,0 +1,32 @@
+import sbevents, sbserver
+import string
+from ConfigParser import ConfigParser
+
+motdstring = ''
+
+def greet(cn):
+	print motdstring
+	sbserver.message(motdstring)
+
+def compilemotd():
+	conf = ConfigParser()
+	if not conf.read('Motd/plugin.conf'):
+		print 'Could not read MOTD config.'
+	if not conf.has_option('MOTD', 'template'):
+		motdstring = 'Welcome to a XSBS server.'
+	else:
+		options = conf.options('MOTD')
+		formatdict = {}
+		for option in options:
+			if option != 'template':
+				formatdict[option] = conf.get('MOTD', option)
+		motdstring = string.Template(conf.get('MOTD', 'template'))
+		motdstring = motdstring.substitute(formatdict)
+		return motdstring
+
+def init():
+	sbevents.registerEventHandler("player_active", greet)
+
+init()
+motdstring = compilemotd()
+
