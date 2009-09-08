f36e9bf0b12a7c28934d60d5beacace5ef113b5a
Gregory Haynes <greg@greghaynes.net>
2009-09-08 00:24:10 +0000

    Fixed banner time to 3 mins

--- src/pyscripts/Banner/banner.py
+++ src/pyscripts/Banner/banner.py
@@ -3,7 +3,7 @@ import sbserver, sbevents, sbtools
 def showBanner():
 	sbserver.message(sbtools.yellow('This is a ' + sbtools.blue('XSBS') + ' server.'))
 	sbserver.message(sbtools.yellow('For information about ' + sbtools.blue('XSBS') + ' join ' + sbtools.orange('#xsbs') + ' on ' + sbtools.orange('irc.gamesurge.net') + '.'))
-	sbevents.timerman.addTimer(18000, showBanner, ())
+	sbevents.timerman.addTimer(180000, showBanner, ())
 
 showBanner()
 
