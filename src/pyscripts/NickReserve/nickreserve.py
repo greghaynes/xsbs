b6ae94657c1c16f3063a91f19969ce0e9a710411
Gregory Haynes <greg@greghaynes.net>
2009-09-09 23:31:43 +0000

    Added nick reserve plugin

---
+++ src/pyscripts/NickReserve/nickreserve.py
@@ -0,0 +1,54 @@
+from DB.db import dbmanager
+from UserManager.usermanager import session, NickAccount, isLoggedIn, loggedInAs
+from Bans.bans import ban
+import sbserver, sbevents, sbtools
+
+def warnNickReserved(cn, nickacct, count):
+	if cn not in sbserver.clients():
+		return
+	nick = nickacct.nick
+	if nick != sbserver.playerName(cn):
+		onPlayerActive(cn)
+		return
+	if isLoggedIn(cn):
+		user = loggedInAs(cn)
+		if nickacct.user_id != user.id:
+			ban(cn, 0, 'Use of reserved name')
+		return
+	if count > 4:
+		ban(cn, 0, 'Use of reserved name')
+		return
+	remaining = 25-(count*5)
+	sbserver.playerMessage(cn, sbtools.red('WARNING: ') + sbtools.blue('Your name is reserved. You have %i seconds to login or be kicked.' % remaining))
+	sbevents.registerTimerHandler(5000, warnNickReserved, (cn, nickacct, count+1))
+
+def onPlayerActive(cn):
+	nick = sbserver.playerName(cn)
+	nickacct = session.query(NickAccount).filter(NickAccount.nick==nick).one()
+	if not nickacct:
+		print 'not reserved'
+		return
+	warnNickReserved(cn, nickacct, 0)
+
+def onPlayerNameChanged(cn, new_name):
+	onPlayerActive(cn)
+
+def onReserveCommand(cn, args):
+	if args != '':
+		sbserver.playerMessage(sbtools.red('Usage: #reserve'))
+		return
+	nick = sbserver.playerName(cn)
+	if not isLoggedIn(cn):
+		sbserver.playerMessage(sbtools.red('You must be logged in to reserve your name.'))
+		return
+	user = loggedInAs(cn)
+	if reserver(nick):
+		sbserver.playerMessage(sbtools.red('Name is already reserved.'))
+		return
+	rn = ReservedNick(nick, user.id)
+	session.add(rn)
+
+sbevents.registerEventHandler('player_active', onPlayerActive)
+sbevents.registerEventHandler('player_name_changed', onPlayerNameChanged)
+sbevents.registerCommandHandler('reserve', onReserveCommand)
+
