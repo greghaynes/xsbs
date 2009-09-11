b111ffa12709a9aced1c7979a187738565aa3974
Gregory Haynes <greg@greghaynes.net>
2009-09-11 05:59:59 -0700

    Added Help plugin for command help information

---
+++ src/pyscripts/Help/help.py
@@ -0,0 +1,60 @@
+import sbserver, sbevents, sbtools
+
+helptexts = {
+	'help': (True,
+		('#help <command>',),
+		(
+			'View information about a command',
+			'Command arguments enclosed in <brackets> are required.',
+			'Command arguments enclosed in (parenthesis) are optional.'
+		)),
+	'stats': (True,
+		('#stats (cn)','#stats (name)'),
+		('Display current game statistics of a player (or self).',)
+		),
+	'givemaster': (False,
+		('#givemaster <cn>', '#givemaster <name>'),
+		('Give master to a player.  You must be master or admin to use this command.',)
+		),
+	'giveadmin': (False,
+		('#giveadmin <cn>', '#giveadmin <name>'),
+		('Give admin to a player.  You must be admin to use this command.',)
+		),
+}
+
+### DO NOT MODIFY BELOW HERE ###
+# UNLESS YOU HAVE SUPER POWERS #
+
+available_commands_str = sbtools.blue('Available commands: ')
+available_commands_str += sbtools.orange()
+for command in helptexts.items():
+	if command[1][0]:
+		available_commands_str += '#' + command[0] + ' '
+
+def onPlayerActive(cn):
+	sbserver.playerMessage(cn, available_commands_str)
+
+def msgHelpText(cn, cmd):
+	try:
+		helpinfo = helptexts[cmd]
+	except KeyError:
+		sbserver.playerMessage(cn, sbtools.red('Command not found'))
+	else:
+		msgs = []
+		for usage in helpinfo[1]:
+			msgs.append(sbtools.red(usage))
+		for desc in helpinfo[2]:
+			msgs.append(sbtools.blue(desc))
+		for msg in msgs:
+			sbserver.playerMessage(cn, msg)
+
+def onHelpCommand(cn, args):
+	args = args.split(' ')
+	if len(args) != 1:
+		msgHelpText(cn, 'help')
+	else:
+		msgHelpText(cn, args[0])
+
+sbevents.registerEventHandler('player_active', onPlayerActive)
+sbevents.registerCommandHandler('help', onHelpCommand)
+
