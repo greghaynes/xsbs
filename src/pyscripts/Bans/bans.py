3fb7e219f53ab675f047a6be06c8f02e8fdc769a
Gregory Haynes <greg@greghaynes.net>
2009-09-12 14:06:02 -0700

    Fixed bans to use colordict and new command API

--- src/pyscripts/Bans/bans.py
+++ src/pyscripts/Bans/bans.py
@@ -1,4 +1,4 @@
-import sbserver, sbevents
+import sbserver, sbevents, sbtools
 from settings import PluginConfig
 import time, string
 
@@ -7,37 +7,39 @@ banned_ips = {}
 config = PluginConfig('bans')
 ban_command = config.getOption('Config', 'ban_command', 'ban')
 default_ban_length = config.getOption('Config', 'default_ban_time', 3600)
-ban_message = config.getOption('Config', 'message', 'Banning $name for $seconds seconds for $red$reason$white.')
+ban_message = config.getOption('Config', 'message', 'Banning $name for $seconds seconds for ${red}${reason}')
 ban_message = string.Template(ban_message)
 default_reason = config.getOption('Config', 'default_reason', 'unspecified reason')
 del config
 
-def onMessage(cn, text):
+def onBanCmd(cn, text):
 	sp = text.split(' ')
-	if len(sp) >=2 and sp[0] == ban_command and sp[1]:
+	try:
 		if sbserver.playerPrivilege(cn) == 0:
-			sbserver.playerMessage(cn, 'Insufficient privileges.')
+			sbserver.playerMessage(cn, sbtools.red('Insufficient privileges.'))
 			return
-		tcn = int(sp[1])
+		tcn = int(sp[0])
 		ip = sbserver.playerIpLong(tcn)
 		if not ip:
 			sbserver.message(cn, 'Invalid cn')
 			return
 		reason = ''
 		length = 0
-		if len(sp) >= 4:
-			reason = sp[3]
-			length = int(sp[2])
-		elif len(sp) == 3:
-			reason = default_reason
-			length = int(sp[2])
+		if len(sp) >= 3:
+			reason = sp[2]
 		else:
-			return
+			reason = default_reason
+		if len(sp) >= 2:
+			length = int(sp[1])
+		else:
+			length = int(default_ban_length)
 		ban(cn, length, reason)
+	except (ValueError, KeyError):
+		sbserver.playerMessage(cn, sbtools.red('Usage: #ban <cn> (duration) (reason)'))
 
 def ban(cn, seconds, reason):
 	ip = sbserver.playerIpLong(cn)
-	msg = ban_message.substitute(name=sbserver.playerName(cn), seconds=seconds, reason=reason)
+	msg = ban_message.substitute(sbtools.colordict, name=sbserver.playerName(cn), seconds=seconds, reason=reason)
 	sbserver.message(msg)
 	expiration = time.time() + seconds
 	if banned_ips.has_key(ip):
@@ -59,7 +61,7 @@ def allowClient(cn):
 
 def init():
 	sbevents.registerPolicyEventHandler("allow_connect", allowClient)
-	sbevents.registerEventHandler("player_command", onMessage)
+	sbevents.registerCommandHandler('ban', onBanCmd)
 	sbevents.registerEventHandler("player_ban", ban)
 
 init()
