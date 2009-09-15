58e0d3f4e6e73a0f79a3d57271d8f9a1aff08072
henriklindgren <oxfile@hotmail.com>
2009-09-15 13:49:27 -0700

    Async version of Translator

--- src/pyscripts/Translator/trans.py
+++ src/pyscripts/Translator/trans.py
@@ -1,21 +1,15 @@
-import sbevents, sbserver, sbtools
-import urllib2, urllib
-jsonlib = None
-try:
-	import json #standard in python 2.6
-	jsonlib = json
-except:
-        try:
-                import simplejson
-                jsonlib =  simplejson
-        except:
-                pass
+import sbserver
+from xsbs.commands import registerCommandHandler
+from xsbs.colors import red
+import socket, re, asyncore
 
 #If Google changes behaviour this script will stop working.
+query=""
+from_lang=""
+to_lang="" 
 
 url = "http://ajax.googleapis.com/ajax/services/language/translate"
-headers = {'User-Agent' : 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.1) Gecko/20061205 Iceweasel/2.0.0.1 (Debian-2.0.0.1+dfsg-2)'}
-langs = ['af','sq','am','ar','hy',
+langslist = ['af','sq','am','ar','hy',
 		'az','eu','be','bn','bh','bg','my',
 		'ca','chr','zh','zh-CN','zh-TW',
 		'hr','cs','da','dv','nl','en',
@@ -31,51 +25,64 @@ langs = ['af','sq','am','ar','hy',
 		'tg','ta','tl','te','th','bo',
 		'tr','uk','ur','uz','ug','vi',
 		'cy','yi']
+host = 'ajax.googleapis.com'
+port = 80
+header = """GET http://ajax.googleapis.com/ajax/services/language/translate?langpair=%s|%s&v=1.0&q=%s HTTP/1.1
 
-class Translator:
-	def __init__(self, url, headers, langs):
-		self.url = url
-		self.headers = headers
-		self.langs = langs
+"""
 
-	def translate(self,word, from_lang = "en", to_lang = "sv"):#Default is english to swedish conversion
-		values = {"langpair":"%s|%s" %(from_lang,to_lang), "ie":"UTF8", "oe":"UTF8", "v":"1.0", "q":word}
-		data = urllib.urlencode(values)
-		request = urllib2.Request(self.url, data, self.headers)
-		response = urllib2.urlopen(request)
-		try:
-			#TODO: character conversion needed here, sauerbraten doesn't handle utf8
-			return "Translation: %s" % ( jsonlib.loads(response.read())["responseData"]["translatedText"] )
-		except Exception:
-			return "Could not translate"
+class SocketDispatch(asyncore.dispatcher):
+	def __init__(self, host, port, header):
+		asyncore.dispatcher.__init__(self)
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.connect((host, port))
+		self.setblocking(0)
+		self.buff = ''
+		self.writebuff = ''
+		begin = 'translatedText\":\"'
+		end = '\"'
+		self.pattern = re.compile(begin+"(.*?)"+end, re.DOTALL)
+	def __del__(self):
+		self.close
+	def handle_connect(self):
+		pass
+	def handle_write(self):
+		sent = self.send(self.writebuff)
+		self.writebuff = self.writebuff[sent:]
+	def writeable(self):
+		return len(self.writebuff) > 0
+	def handle_read(self):
+		self.buff += self.recv(4096)
+		if self.buff != "":
+			m = re.search(self.pattern, self.buff)
+			self.buff = ""
+			try:
+				sbserver.message(m.group(1))
+			except:
+				sbserver.playerMessage(cn,"Translation failed")
+	def write(self,query, from_lang="en", to_lang="sv"):
+		self.writebuff += header % (from_lang, to_lang, query)
+		self.handle_write()
 
-	def is_lang(self,s):
-		for lang in self.langs:
-			if lang.lower() == s.lower():
-				return True
-		return False
-	
-	def sentence(self,list_of_strings):
-		str = ""
-		for s in list_of_strings:
-			str += s + " "
-		return str
+sd = SocketDispatch(host, port, header)
 
-translator = Translator(url, headers, langs)
+def is_lang(s,langs):
+	for lang in langs:
+		if lang.lower() == s.lower():
+			return True
+	return False
 
 def onCommand(cn, command):
 	cmd_list = command.split()
 	length = len(cmd_list)
 	if length == 0:
-		sbserver.playerMessage(cn, sbtools.red("Usage: #trans <word> or #trans <sentence/word> <from_lang> <to_lang> Language codes: en, sv, de, it and lots more."))
+		sbserver.playerMessage(cn, red("Usage: #trans <word> or #trans <sentence/word> <from_lang> <to_lang> Language codes: en, sv, de, it and lots more."))
 	elif length == 1:
-		sbserver.message(translator.translate(cmd_list[0]))
+		sd.write(cmd_list[0])
 	elif length > 2:
-		if translator.is_lang(cmd_list[length-2]) and translator.is_lang(cmd_list[length-1]):
-			translation = translator.translate(translator.sentence(cmd_list[:length-2]),cmd_list[length-2],cmd_list[length-1])
-			sbserver.message(translation)
+		if is_lang(cmd_list[length-2],langslist) and is_lang(cmd_list[length-1],langslist):
+			sd.write("%20".join(cmd_list[:length-2]), cmd_list[length-2], cmd_list[length-1])
+		else:
+			sbserver.playerMessage(cn, red("You've made a mistake with the syntax. Type #trans to show syntax"))
 
-if jsonlib == None:
-	print "Python json library not present. Can't use #trans."
-else:
-	sbevents.registerCommandHandler('trans', onCommand)
+registerCommandHandler('trans', onCommand)
