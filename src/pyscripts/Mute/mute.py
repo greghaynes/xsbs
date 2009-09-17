88cb55c5d339babcb7a2b82f15170e30068ecdb1
Gregory Haynes <greg@greghaynes.net>
2009-09-16 17:18:02 -0700

    Created #mute and #unmute

---
+++ src/pyscripts/Mute/mute.py
@@ -0,0 +1,57 @@
+import sbserver
+from xsbs.colors import red, orange, white
+from xsbs.events import registerPolicyEventHandler, registerServerEventHandler
+from xsbs.commands import registerCommandHandler
+
+muted_players = []
+
+def allowMsg(cn, text):
+	if cn in muted_players:
+		sbserver.playerMessage(cn, red('You are currently muted.  No one will recieve your messages.'))
+		return False
+	return True
+
+def onMuteCommand(cn, args):
+	try:
+		args = args.split(' ')
+		tcn = int(args[0])
+		if len(args) > 1:
+			raise KeyError
+		if tcn not in muted_players:
+			name = sbserver.playerName(tcn)
+			muted_players.append(tcn)
+			sbserver.message(orange(name) + white(' has been muted'))
+		else:
+			sbserver.playerMessage(cn, red('Player is already muted.'))
+	except KeyError:
+		sbserver.playerMessage(red('Usage: #mute <cn>'))
+	except ValueError:
+		sbserver.playerMessage(cn, red('Invalid player cn'))
+
+def onUnmuteCommand(cn, args):
+	try:
+		args = args.split(' ')
+		tcn = int(args[0])
+		if len(args) > 1:
+			raise KeyError
+		if tcn not in muted_players:
+			sbserver.playerMessage(cn, red('Player is not crrently muted'))
+		name = sbserver.playerName(tcn)
+		muted_players.remove(tcn)
+		sbserver.message(orange(name) + white(' has been unmuted'))
+	except KeyError:
+		sbserver.playerMessage(cn, red('Usage: #unmute <cn>'))
+	except ValueError:
+		sbserver.playerMessage(cn, red('Invalid player cn'))
+
+def onDisconnect(cn):
+	try:
+		muted_players.remove(cn)
+	except ValueError:
+		pass
+
+registerPolicyEventHandler('allow_message', allowMsg)
+registerCommandHandler('mute', onMuteCommand)
+registerCommandHandler('unmute', onUnmuteCommand)
+registerServerEventHandler('player_disconnect', onDisconnect)
+
