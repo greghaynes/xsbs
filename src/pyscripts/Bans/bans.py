ac187b6f48df292488a93fdd6174b3cbf0e3a86d
Gregory Haynes <greg@greghaynes.net>
2009-09-20 03:11:47 -0700

    Bans now uses database

--- src/pyscripts/Bans/bans.py
+++ src/pyscripts/Bans/bans.py
@@ -3,9 +3,54 @@ from xsbs.settings import PluginConfig
 from xsbs.colors import red, colordict
 from xsbs.events import triggerServerEvent, registerServerEventHandler, registerPolicyEventHandler, execLater
 from xsbs.commands import registerCommandHandler
+from DB.db import dbmanager
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import NoResultFound
 import time, string
 
-banned_ips = {}
+Base = declarative_base()
+session = dbmanager.session()
+
+class Ban(Base):
+	__tablename__='bans'
+	id = Column(Integer, primary_key=True)
+	ip = Column(Integer)
+	expiration = Column(Integer)
+	reason = Column(String)
+	nick = Column(String)
+	banner_ip = Column(Integer)
+	banner_nick = Column(String)
+	def __init__(self, ip, expiration, reason, nick, banner_ip, banner_nick):
+		self.ip = ip
+		self.expiration = expiration
+		self.reason = reason
+		self.nick = nick
+		self.banner_ip = banner_ip
+		self.banner_nick = banner_nick
+	def isExpired(self):
+		return self.expiration <= time.time()
+
+
+def getCurrentBanByIp(ipaddress):
+	return session.query(Ban).filter(Ban.ip==ipaddress).filter('expiration>'+str(time.time())).one()
+
+def isCurrentlyBanned(ipaddress):
+	try:
+		b = getCurrentBanByIp(ipaddress)
+		return True
+	except NoResultFound:
+		return False
+
+def ban(cn, seconds, reason, banner_cn):
+	ip = sbserver.playerIpLong(cn)
+	expiration = time.time() + seconds
+	nick = sbserver.playerName(cn)
+	banner_ip = sbserver.playerIpLong(banner_cn)
+	banner_nick = sbserver.playerName(banner_cn)
+	newban = Ban(ip, expiration, reason, nick, banner_ip, banner_nick)
+	session.add(newban)
+	execLater(sbserver.playerKick, (cn,))
 
 config = PluginConfig('bans')
 ban_command = config.getOption('Config', 'ban_command', 'ban')
@@ -37,36 +82,18 @@ def onBanCmd(cn, text):
 			length = int(sp[1])
 		else:
 			length = int(default_ban_length)
-		ban(tcn, length, reason)
+		ban(tcn, length, reason, cn)
 	except (ValueError, KeyError):
 		sbserver.playerMessage(cn, red('Usage: #ban <cn> (duration) (reason)'))
 
-def ban(cn, seconds, reason):
-	ip = sbserver.playerIpLong(cn)
-	msg = ban_message.substitute(colordict, name=sbserver.playerName(cn), seconds=seconds, reason=reason)
-	sbserver.message(msg)
-	expiration = time.time() + seconds
-	if banned_ips.has_key(ip):
-		if banned_ips[ip] > expiration:
-			execLater(sbserver.playerKick, (cn,))
-			return
-	banned_ips[ip] = expiration
-	triggerServerEvent('player_banned', (cn,seconds, reason))
-	execLater(sbserver.playerKick, (cn,))
-
 def allowClient(cn):
 	ip = sbserver.playerIpLong(cn)
-	if banned_ips.has_key(ip):
-		expiration = banned_ips[ip]
-		if time.time() >= expiration:
-			banned_ips.pop(ip)
-			return True
-		return False
-	return True
+	return not isCurrentlyBanned(ip)
 
 def init():
 	registerPolicyEventHandler("allow_connect", allowClient)
 	registerCommandHandler('ban', onBanCmd)
 	registerServerEventHandler("player_ban", ban)
+	Base.metadata.create_all(dbmanager.engine)
 
 init()
