c2d159d96b62930c686c31df8105045433db3d3e
Gregory Haynes <greg@greghaynes.net>
2009-09-21 03:44:53 -0700

    Config file and template for teamkill message

--- src/pyscripts/GameNotifications/notify.py
+++ src/pyscripts/GameNotifications/notify.py
@@ -1,10 +1,19 @@
 import sbserver
 from xsbs.colors import red
 from xsbs.ui import info
+from xsbs.colors import colordict
 from xsbs.events import registerServerEventHandler
+from xsbs.settings import PluginConfig
+import string
+
+config = PluginConfig('gamenotifications')
+tktemp = config.getOption('Config', 'teamkill', '${red}Player ${green}${tker}${red} has team killed ${orange}${victim}')
+del config
+
+tktemp = string.Template(tktemp)
 
 def teamkill(cn, tcn):
-	info(red('Player %s has teamkilled %s' % (sbserver.playerName(cn), sbserver.playerName(tcn))))
+	info(tktemp.substitute(colordict, tker=sbserver.playerName(cn), victim=sbserver.playerName(tcn)))
 
 def getmap(cn):
 	info('Player %s is downloading map' % sbserver.playerName(cn))
