799e78433f5325cb3b354cedcf068c80eec6999c
Gregory Haynes <greg@greghaynes.net>
2009-09-20 18:31:55 -0700

    Added #givemaster

--- src/pyscripts/ServerCtlCommands/ctl.py
+++ src/pyscripts/ServerCtlCommands/ctl.py
@@ -1,6 +1,6 @@
 import sbserver
 from xsbs.commands import registerCommandHandler
-from xsbs.colors import red, yellow, blue
+from xsbs.colors import red, yellow, blue, green
 from xsbs.plugins import reload as pluginReload
 from Motd.motd import motdstring
 
@@ -29,8 +29,24 @@ def onReloadCmd(cn, args):
 def onInfoCmd(cn, args):
 	sbserver.playerMessage(cn, motdstring)
 
+def onGiveMaster(cn, args):
+	if args == '':
+		sbserver.playerMessage(cn, red('Usage #givemaster <cn>'))
+		return
+	try:
+		tcn = int(args)
+	except TypeError:
+		sbserver.playerMessage(cn, red('Usage #givemaster <cn>'))
+		return
+	if sbserver.playerPrivilege(cn) > 0:
+		sbserver.playerMessage(cn, green('You have given master to %s') % sbserver.playerName(tcn))
+		sbserver.setMaster(tcn)
+	else:
+		sbserver.playerMessage(cn, red('Insufficient permissions.'))
+
 registerCommandHandler('pause', onPauseCmd)
 registerCommandHandler('resume', onResumeCmd)
 registerCommandHandler('reload', onReloadCmd)
 registerCommandHandler('info', onInfoCmd)
+registerCommandHandler('givemaster', onGiveMaster)
 
