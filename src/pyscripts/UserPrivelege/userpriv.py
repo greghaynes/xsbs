ffcd234d021f7926e10865074388cff8724758fc
Gregory Haynes <greg@greghaynes.net>
2009-09-21 19:38:46 -0700

    Added base UserPrivilege plugin

---
+++ src/pyscripts/UserPrivelege/userpriv.py
@@ -0,0 +1,52 @@
+import sbserver
+from DB.db import dbmanager
+from UserManager.usermanager import User, loggedInAs
+from xsbs.commands import registerCommandHandler
+from xsbs.ui import error, insufficientPermissions
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relation
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import NoResultFound
+
+Base = declarative_base()
+session = dbmanager.session()
+
+USER = 0
+MASTER = 1
+ADMIN = 2
+
+class UserPrivilege(Base):
+	__tablename__ = 'userprivileges'
+	id = Column(Integer, primary_key=True)
+	privilege = Column(Integer)
+	user_id = Column(Integer)
+	def __init__(self, privilege, user_id):
+		self.privilege = privilege
+		self.user_id = user_id
+
+def isMaster(user_id):
+	try:
+		priv = session.query(UserPrivilege).filter(UserPrivilege.user_id==user_id).filter(UserPrivilege.privilege==USER).one()
+		return True
+	except NoResultFound:
+		return False
+
+def isPlayerMaster(cn):
+	try:
+		user = loggedInAs(cn)
+		if isMaster(user.id):
+			sbserver.setMaster(cn)
+		else:
+			insufficientPermissions(cn)
+	except AttributeError:
+		sbserver.playerMessage(cn, error('You need to verify using /setmaster before using #master'))
+
+def masterCmd(cn, args):
+	return isPlayerMaster(cn)
+
+def init():
+	registerCommandHandler('master', masterCmd)
+	Base.metadata.create_all(dbmanager.engine)
+
+init()
+
