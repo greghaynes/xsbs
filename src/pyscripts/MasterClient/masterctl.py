712d482aa1538acd53d655ae3d64c91ef914258d
Gregory Haynes <greg@greghaynes.net>
2009-10-02 00:15:59 -0700

    Dont leave master connection open

--- src/pyscripts/MasterClient/masterctl.py
+++ src/pyscripts/MasterClient/masterctl.py
@@ -12,22 +12,19 @@ class MasterClient(asyncore.dispatcher):
 		asyncore.dispatcher.__init__(self)
 		self.hostname = hostname
 		self.port = port
-		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.buff = ''
 		self.out_buff = []
 		self.is_registered = False
 		self.is_connected = False
 		self.next_auth_id = 0
 		self.auth_map = {}
-		self.connect((self.hostname, self.port))
+		self.do_connect()
 		self.register()
 	def handle_close(self):
 		self.is_connected = False
 		self.close()
-		print 'Master connection closed'
 	def handle_connect(self):
 		self.is_connected = True
-		print 'Connected to master server'
 		addTimer(3600000, self.update)
 	def handle_write(self):
 		for out in self.out_buff:
@@ -35,6 +32,11 @@ class MasterClient(asyncore.dispatcher):
 		del self.out_buff[:]
 	def writable(self):
 		return len(self.out_buff) > 0
+	def do_connect(self):
+		if not self.is_connected:
+			self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.connect((self.hostname, self.port))
+			self.is_connected = True
 	def handle_read(self):
 		self.buff += self.recv(4096)
 		tmp_buff = self.buff.split('\n')
@@ -45,15 +47,21 @@ class MasterClient(asyncore.dispatcher):
 			if key == 'failreg':
 				self.is_registered = False
 				print 'Failed to register with master server'
+				self.is_connected = False
+				self.close()
 			elif key == 'succreg':
 				self.is_registered = True
 				print 'Successfully registered with master server'
+				self.is_connected = False
+				self.close()
 			elif key == 'chalauth':
 				cn = self.auth_map[int(args[1])][0]
 				chal = args[2]
 				sbserver.authChallenge(cn, int(args[1]), chal)
 			elif key == 'failauth':
 				del self.auth_map[int(args[1])]
+				self.close()
+				self.is_connected = False
 			elif key == 'succauth':
 				authtup = self.auth_map[int(args[1])]
 				cn = authtup[0]
@@ -62,24 +70,21 @@ class MasterClient(asyncore.dispatcher):
 				sbserver.setMaster(cn)
 				sbserver.message(info(claimstr % (nick, authname)))
 				del self.auth_map[int(args[1])]
+				self.is_connected = False
+				self.close()
 	def register(self):
+		self.do_connect()
 		self.out_buff.append('regserv %i\n' % sbserver.port())
 	def update(self):
-		if not self.is_connected:
-			self.connect()
 		self.register()
 	def tryauth(self, cn, name):
-		if not self.is_connected:
-			sbserver.playerMessage(cn, error('Not connected to an authentication server'))
-		else:
-			self.auth_map[self.next_auth_id] = (cn, name)
-			self.out_buff.append('reqauth %i %s\n' % (self.next_auth_id, name))
-			self.next_auth_id += 1
+		self.do_connect()
+		self.auth_map[self.next_auth_id] = (cn, name)
+		self.out_buff.append('reqauth %i %s\n' % (self.next_auth_id, name))
+		self.next_auth_id += 1
 	def anschal(self, id, val):
-		if not self.is_connected:
-			sbserver.playerMessage(cn, error('You should not be answering a challenge right now'))
-		else:
-			self.out_buff.append('confauth %i %s\n' % (id, val))
+		self.do_connect()
+		self.out_buff.append('confauth %i %s\n' % (id, val))
 
 mc = MasterClient()
 registerServerEventHandler('auth_try', mc.tryauth)
