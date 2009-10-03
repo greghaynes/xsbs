80c3351cbd61cdbbd8cde481e16256db9223d287
Gregory Haynes <greg@greghaynes.net>
2009-10-03 01:58:17 -0700

    GeoIp messages uses info()

--- src/pyscripts/GeoIp/geoip.py
+++ src/pyscripts/GeoIp/geoip.py
@@ -1,6 +1,7 @@
 import sbserver
 from xsbs.settings import PluginConfig
 from xsbs.colors import colordict
+from xsbs.ui import info
 from xsbs.events import registerServerEventHandler
 from xsbs.net import ipLongToString
 import string
@@ -9,7 +10,7 @@ import pygeoip
 db = pygeoip.Database('GeoIp/GeoIP.dat')
 
 conf = PluginConfig('geoip')
-template = '${green}${user} ${yellow}has connected from ${orange}${country}'
+template = '${green}${user} ${white}has connected from ${orange}${country}'
 template = conf.getOption('Config', 'template', template)
 del conf
 
@@ -18,7 +19,7 @@ def getCountry(ip):
 
 def announce(cn):
 	msg = string.Template(template).substitute(colordict, user=sbserver.playerName(cn), country=getCountry(sbserver.playerIpLong(cn)))
-	sbserver.message(msg)
+	sbserver.message(info(msg))
 
 registerServerEventHandler("player_connect_delayed", announce)
 
