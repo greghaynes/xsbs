51a05fc2b4717860fea1556d7f80223864ffd3c7
Gregory Haynes <greg@greghaynes.net>
2009-10-03 15:58:44 -0700

    Allow force map if mm > 0

--- src/pyscripts/MapVote/mapvote.py
+++ src/pyscripts/MapVote/mapvote.py
@@ -42,7 +42,7 @@ mapreload = [False]
 def onMapSet(cn, mapname, mapmode):
 	if sbserver.mapName() == '':
 		sbserver.setMap(mapname, mapmode)
-	elif sbserver.playerPrivilege(cn) > 0 and sbserver.masterMode() > 1:
+	elif sbserver.playerPrivilege(cn) > 0 and sbserver.masterMode() > 0:
 		sbserver.setMap(mapname, mapmode)
 	elif mapmode != sbserver.gameMode() and not allow_modevote and sbserver.playerPrivilege(cn) == 0:
 		sbserver.playerMessage(cn, error('You cannot request a new game mode'))
