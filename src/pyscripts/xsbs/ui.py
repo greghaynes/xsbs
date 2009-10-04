2cf00dd517b87b2f6f95e038f333d28688d9e61f
Gregory Haynes <greg@greghaynes.net>
2009-10-04 02:09:27 -0700

    Message prefixes configurable

--- src/pyscripts/xsbs/ui.py
+++ src/pyscripts/xsbs/ui.py
@@ -1,17 +1,31 @@
 import sbserver
-from xsbs.colors import orange, yellow, red, blue
+from xsbs.colors import colordict
+from xsbs.settings import PluginConfig
+import string
+
+config = PluginConfig('ui')
+notice_pre = config.getOption('Prefixes', 'notice', '${blue}Notice:')
+info_pre = config.getOption('Prefixes', 'info', '${yellow}Info:')
+warning_pre = config.getOption('Prefixes', 'warning', '${red}Warning:')
+error_pre = config.getOption('Prefixes', 'error', '${red}Error:')
+del config
+
+notice_pre = string.Template(notice_pre).substitute(colordict) + ' '
+info_pre = string.Template(info_pre).substitute(colordict) + ' '
+warning_pre = string.Template(warning_pre).substitute(colordict) + ' '
+error_pre = string.Template(error_pre).substitute(colordict) + ' '
 
 def notice(message):
-	return blue('Notice: ') + message
+	return notice_pre + message
 
 def info(message):
-	return yellow('Info: ') + message
+	return info_pre + message
 
 def warning(message):
-	return red('Warning: ') + message
+	return warning_pre + message
 
 def error(message):
-	return red('Error: ') + message
+	return error_pre + message
 
 def insufficientPermissions(cn):
 	sbserver.playerMessage(cn, error('Insufficient permissions'))
