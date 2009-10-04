ea4c93e0c57e7ba86f45025608678dc97f89b44c
Gregory Haynes <greg@greghaynes.net>
2009-10-04 02:22:16 -0700

    GeoIP uses get_country_by_addr, not by_name

--- src/pyscripts/GeoIp/geoip.py
+++ src/pyscripts/GeoIp/geoip.py
@@ -15,7 +15,7 @@ template = conf.getOption('Config', 'template', template)
 del conf
 
 def getCountry(ip): 
-	country = db.country_name_by_name(ipLongToString(ip))
+	country = db.country_name_by_addr(ipLongToString(ip))
 	if country == '':
 		country = 'Unknown'
 	return country
