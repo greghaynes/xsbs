819133fd6dcb8d7cbf192f08adc0bd69035d9822
Gregory Haynes <greg@greghaynes.net>
2009-10-10 23:25:30 -0700

    added game.py with modes list

---
+++ src/pyscripts/xsbs/game.py
@@ -0,0 +1,18 @@
+modes = [
+	'ffa',
+	'coop',
+	'team',
+	'insta',
+	'instateam',
+	'effic',
+	'efficteam',
+	'tac',
+	'tacteam',
+	'capture',
+	'regencapture',
+	'ctf',
+	'instactf',
+	'protect',
+	'instaprotect'
+]
+
