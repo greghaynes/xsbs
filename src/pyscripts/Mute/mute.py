cb3bf961f193dcfd4a5f98823a0e75ad19114a48
Gregory Haynes <greg@greghaynes.net>
2009-10-16 03:23:41 -0700

    Updated mute plugin

--- src/pyscripts/Mute/mute.py
+++ src/pyscripts/Mute/mute.py
@@ -1,14 +1,26 @@
 import sbserver
-from xsbs.colors import red, orange, white
+from xsbs.colors import colordict
+from xsbs.ui import info, notice, error
 from xsbs.events import registerPolicyEventHandler, registerServerEventHandler
+from xsbs.players import player
+from xsbs.settings import PluginConfig
 from UserPrivelege.userpriv import registerCommandHandler, masterRequired
+import string
 
-muted_players = []
+config = PluginConfig('mute')
+muted_temp = config.getOption('Messages', 'muted_message', '${green}${muted_name}${white} has been ${red}muted')
+unmuted_temp = config.getOption('Messages', 'unmuted_message', '${green}${muted_name}${white} has been ${red}unmuted')
+del config
+muted_temp = string.Template(muted_temp)
+unmuted_temp = string.Template(unmuted_temp)
 
 def allowMsg(cn, text):
-	if cn in muted_players:
-		sbserver.playerMessage(cn, red('You are currently muted.  No one will recieve your messages.'))
-		return False
+	try:
+		if player(cn).is_muted:
+			sbserver.playerMessage(cn, notice('You are currently muted.  No one will recieve your messages.'))
+			return False
+	except AttributeError:
+		pass
 	return True
 
 @masterRequired
@@ -18,17 +30,24 @@ def onMuteCommand(cn, args):
 		tcn = int(args[0])
 		if len(args) > 1:
 			raise KeyError
-		if tcn not in muted_players:
-			name = sbserver.playerName(tcn)
-			muted_players.append(tcn)
-			sbserver.message(orange(name) + white(' has been muted'))
+		try:	
+			p = player(tcn)
+		except ValueError:
+			sbserver.playerMessage(cn, errer('Invalid player cn'))
 		else:
-			sbserver.playerMessage(cn, red('Player is already muted.'))
+			try:
+				muted = p.is_muted
+			except AttributeError:
+			 	muted = False
+			if muted:
+				sbserver.playerMessage(cn, error('Player is already muted.'))
+			else:
+				p.is_muted = True
+				name = sbserver.playerName(tcn)
+				sbserver.message(info(muted_temp.substitute(colordict, muted_name=name)))
 	except KeyError:
-		sbserver.playerMessage(red('Usage: #mute <cn>'))
-	except ValueError:
-		sbserver.playerMessage(cn, red('Invalid player cn'))
-
+		sbserver.playerMessage(cn, error('Usage: #mute <cn>'))
+			
 @masterRequired
 def onUnmuteCommand(cn, args):
 	try:
@@ -36,15 +55,15 @@ def onUnmuteCommand(cn, args):
 		tcn = int(args[0])
 		if len(args) > 1:
 			raise KeyError
-		if tcn not in muted_players:
-			sbserver.playerMessage(cn, red('Player is not crrently muted'))
-		name = sbserver.playerName(tcn)
-		muted_players.remove(tcn)
-		sbserver.message(orange(name) + white(' has been unmuted'))
+		if player(tcn).is_muted:
+			player(tcn).is_muted = False
+			sbserver.message(info(unmuted_temp.substitute(colordict, muted_name=sbserver.playerName(tcn))))
+		else:
+			sbserver.playerMessage(cn, error('Player is not crrently muted'))
 	except KeyError:
-		sbserver.playerMessage(cn, red('Usage: #unmute <cn>'))
+		sbserver.playerMessage(cn, error('Usage: #unmute <cn>'))
 	except ValueError:
-		sbserver.playerMessage(cn, red('Invalid player cn'))
+		sbserver.playerMessage(cn, error('Invalid player cn'))
 
 def onDisconnect(cn):
 	try:
