ca73a203519a31d56dacb232a42b8d898921a37d
Gregory Haynes <greg@greghaynes.net>
2009-10-24 14:32:18 -0700

    Removed team switch debug message

--- src/pyscripts/TeamController/teamctl.py
+++ src/pyscripts/TeamController/teamctl.py
@@ -16,7 +16,6 @@ setteam_modes = [
 
 def onSwitchTeam(cn, team):
 	mode =  sbserver.gameMode()
-	print 'switch team %i %s (%i)' % (cn, team, mode)
 	if mode in setteam_modes:
 		sbserver.setTeam(cn, team)
 	elif mode in switchteam_modes:
