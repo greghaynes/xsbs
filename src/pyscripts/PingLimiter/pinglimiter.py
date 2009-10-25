d648101a203ec56b17bae9386c81f57579f1405e
Gregory Haynes <greg@greghaynes.net>
2009-10-25 01:24:30 -0700

    Added config to ping limiter

--- src/pyscripts/PingLimiter/pinglimiter.py
+++ src/pyscripts/PingLimiter/pinglimiter.py
@@ -1,10 +1,17 @@
 import sbserver
 from xsbs.timers import addTimer
 from Bans.bans import ban
+from xsbs.settings import PluginConfig
+from xsbs.ui import warning
+
+config = PluginConfig('pinglimiter')
+max_ping = config.getOption('Config', 'max_ping', '600')
+del config
+max_ping = int(max_ping)
 
 class PingLimiter:
-	def __init__(self):
-		self.max_ping = 600 
+	def __init__(self, max_ping):
+		self.max_ping = max_ping
 		self.warned_cns = []
 	def checkPlayers(self):
 		laggers = []
@@ -17,11 +24,11 @@ class PingLimiter:
 				ban(cn, 0, 'lagging')
 				del laggers[i]
 			else:
-				sbserver.playerMessage(cn, 'Your ping is too high.  You will be kicked if it is not lowered.')
+				sbserver.playerMessage(cn, warning('Your ping is too high.  You will be kicked if it is not lowered.'))
 				self.warned_cns.append(lagger)
 				i += 1
 		addTimer(5000, limiter.checkPlayers, ())
 
-limiter = PingLimiter()
+limiter = PingLimiter(max_ping)
 addTimer(5000, limiter.checkPlayers, ())
 
