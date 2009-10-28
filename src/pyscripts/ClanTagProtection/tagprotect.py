71abd23fd0bc82abe7b0e60b9f6e56087062f73a
Gregory Haynes <greg@greghaynes.net>
2009-10-28 03:12:48 -0700

    timers != addTimer

--- src/pyscripts/ClanTagProtection/tagprotect.py
+++ src/pyscripts/ClanTagProtection/tagprotect.py
@@ -2,7 +2,7 @@ from xsbs.events import registerServerEventHandler, execLater
 from xsbs.players import player
 from xsbs.ui import warning
 from xsbs.colors import red
-from xsbs.timers import timers
+from xsbs.timers import addTimer
 from Bans.bans import ban
 from UserManager.usermanager import User
 from NickReserve.nickreserve import nickReserver
