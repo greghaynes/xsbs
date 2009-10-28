66082d1ad214d5ac9f6d33a25d41cb54ff7a1734
Gregory Haynes <greg@greghaynes.net>
2009-10-28 14:34:02 -0700

    Created #servermsg

--- src/pyscripts/ServerCtlCommands/ctl.py
+++ src/pyscripts/ServerCtlCommands/ctl.py
@@ -1,11 +1,18 @@
 import sbserver
 from xsbs.events import triggerServerEvent
 from xsbs.commands import registerCommandHandler
+from xsbs.settings import PluginConfig
 from UserPrivilege.userpriv import masterRequired, adminRequired
-from xsbs.colors import red, yellow, blue, green
+from xsbs.colors import red, yellow, blue, green, colordict
 from xsbs.plugins import reload as pluginReload
 from xsbs.ui import error, info, insufficientPermissions
 from Motd.motd import motdstring
+import string
+
+config = PluginConfig('servercommands')
+servermsg_template = config.getOption('Templates', 'servermsg', '${orange}${sender}${white}: ${message}')
+del config
+servermsg_template = string.Template(servermsg_template)
 
 @masterRequired
 def onPauseCmd(cn, args):
@@ -82,6 +89,14 @@ def unspecAll(cn, args):
 		for s in sbserver.spectators():
 			sbserver.unspectate(s)
 
+@masterRequired
+def serverMessage(cn, args):
+	if args == '':
+		sbserver.playerMessage(cn, error('Usage: #servermsg <message>'))
+	else:
+		msg = servermsg_template.substitute(colordict, sender=sbserver.playerName(cn), message=args)
+		sbserver.message(msg)
+
 registerCommandHandler('pause', onPauseCmd)
 registerCommandHandler('resume', onResumeCmd)
 registerCommandHandler('reload', onReloadCmd)
@@ -91,4 +106,5 @@ registerCommandHandler('resize', onResize)
 registerCommandHandler('minsleft', onMinsLeft)
 registerCommandHandler('specall', specAll)
 registerCommandHandler('unspecall', unspecAll)
+registerCommandHandler('servermsg', serverMessage)
 
