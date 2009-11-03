2dbe65624728fed08d8e0f1c45b54fbd55d59c56
Gregory Haynes <greg@greghaynes.net>
2009-11-03 09:22:02 -0800

    Dont emit teamkill event on suicide

--- src/fpsgame/server.cpp
+++ src/fpsgame/server.cpp
@@ -1119,7 +1119,8 @@ namespace server
             if(isteam(actor->team, target->team))
             {
                 actor->state.teamkills++;
-                SbPy::triggerEventIntInt("player_teamkill", actor->clientnum, target->clientnum);
+				if(actor->clientnum != target->clientnum)
+                	SbPy::triggerEventIntInt("player_teamkill", actor->clientnum, target->clientnum);
             }
             int fragvalue = smode ? smode->fragvalue(target, actor) : (target==actor || isteam(target->team, actor->team) ? -1 : 1);
             actor->state.frags += fragvalue;
