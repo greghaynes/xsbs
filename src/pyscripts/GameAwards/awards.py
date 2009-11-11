47fafc58cdb2fea78bf890612bbccf06f6f619bf
Gregory Haynes <greg@greghaynes.net>
2009-11-10 17:05:08 -0800

    Added most deants award, GameAwards uses player API

--- src/pyscripts/GameAwards/awards.py
+++ src/pyscripts/GameAwards/awards.py
@@ -1,5 +1,5 @@
 from xsbs.events import registerServerEventHandler
-from xsbs.players import all as allPlayers
+from xsbs.players import player, all as allPlayers
 from xsbs.settings import PluginConfig
 from xsbs.colors import colordict
 import sbserver
@@ -9,10 +9,12 @@ config = PluginConfig('gameawards')
 awards_prefix = config.getOption('Config', 'awards_prefix', '${blue}Awards: ${white}')
 mftemp = config.getOption('Config', 'most_frags', 'Most Frags: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
 mtktemp = config.getOption('Config', 'most_teamkills', 'Most TeamKills: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
+mdtemp = config.getOption('Config', 'most_deaths', 'Most Deaths: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
 del config
 awards_prefix = string.Template(awards_prefix).substitute(colordict)
 mftemp = string.Template(mftemp)
 mtktemp = string.Template(mtktemp)
+mdtemp = string.Template(mdtemp)
 
 def onIntermission():
 	players = allPlayers()
@@ -20,25 +22,33 @@ def onIntermission():
 	most_frags_cn = 0
 	most_tks = 0
 	most_tks_cn = -1
-	for player in players:
+	most_deaths = 0
+	most_deaths_cn = -1
+	for p in players:
 		try:
-			frags = sbserver.playerFrags(player.cn)
-			teamkills = sbserver.playerTeamkills(player.cn)
+			frags = p.frags()
+			teamkills = p.teamkills()
+			deaths = p.deaths()
 			if frags > most_frags or most_frags_cn == -1:
-				most_frags = sbserver.playerFrags(player.cn)
-				most_frags_cn = player.cn
+				most_frags = frags
+				most_frags_cn = p.cn
 			if teamkills > most_tks or most_tks_cn == -1:
-				most_tks = sbserver.playerTeamkills(player.cn)
-				most_tks_cn = player.cn
+				most_tks = teamkills
+				most_tks_cn = p.cn
+			if deaths > most_deaths or most_deaths_cn == -1:
+				most_deaths = deaths
+				most_deaths_cn = p.cn
 		except ValueError:
 			continue
 	msg = ''
 	if most_frags > 0:
-		msg += mftemp.substitute(colordict, name=sbserver.playerName(most_frags_cn), count=most_frags)
+		msg += mftemp.substitute(colordict, name=player(most_frags_cn).name(), count=most_frags)
 		msg += ' '
 	if most_tks > 0:
-		msg += mtktemp.substitute(colordict, name=sbserver.playerName(most_tks_cn), count=most_tks)
+		msg += mtktemp.substitute(colordict, name=playerName(most_tks_cn).name(), count=most_tks)
 		msg += ' '
+	if most_deaths > 0:
+		msg += mdtemp.substitute(colordict, name=player(most_tks_cn).name(), count=most_deaths)
 	if msg != '':
 		msg = awards_prefix + msg
 		sbserver.message(msg)
