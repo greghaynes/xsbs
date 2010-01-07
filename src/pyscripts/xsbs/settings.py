7f83aa6ee2b639ce791665e2affff0e793d80658
Gregory Haynes <greghaynes@host-246-178.pubnet.pdx.edu>
2010-01-07 14:25:20 -0800

    Added ip, port, adminPassword to xsbs.settings

--- src/pyscripts/xsbs/settings.py
+++ src/pyscripts/xsbs/settings.py
@@ -55,3 +55,13 @@ class PluginConfig:
 	def configPath(self):
 		return configuration_path + self.plugin_name + configuration_extension
 
+def adminPassword():
+	'''Server administrator password'''
+	return sbserver.adminPassword()
+
+def ip():
+	'''Server listen ip address'''
+
+def port():
+	'''Server listen port'''
+
