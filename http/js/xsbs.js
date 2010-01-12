1933d752d754d9a65d94a906a5db23e4f8467d93
Gregory Haynes <greg@greghaynes.net>
2010-01-12 01:39:47 -0800

    Created foreachClientState

--- http/js/xsbs.js
+++ http/js/xsbs.js
@@ -1,13 +1,13 @@
 function getClientState(hostname, cn, callback) {
 	$.getJSON('http://' + hostname + '/json/clients/' + cn, function(data) {
 		callback(data);
-	});
+		});
 }
 
-function foreachClient(hostname, callback) {
-	getClients(hostname, function(clients) {
-		$.each(clients, function(i, client) {
-			callback(client)
+function foreachClientState(hostname, callback) {
+	foreachClient(hostname, function(client) {
+		getClientState(hostname, client.cn, function(state) {
+			callback(state);
 			});
 		});
 }
@@ -15,6 +15,14 @@ function foreachClient(hostname, callback) {
 function getClients(hostname, callback) {
 	$.getJSON('http://' + hostname + '/json/clients', function(data) {
 		callback(data);
+		});
+}
+
+function foreachClient(hostname, callback) {
+	getClients(hostname, function(clients) {
+		$.each(clients, function(i, client) {
+			callback(client)
+			});
 		});
 }
 
