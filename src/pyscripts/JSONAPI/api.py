7fc93bdcfddf4ede0b84c52816b89d0f444b5c81
Gregory Haynes <greg@greghaynes.net>
2010-01-11 16:35:45 -0800

    Added json/clients/\d+ url

--- src/pyscripts/JSONAPI/api.py
+++ src/pyscripts/JSONAPI/api.py
@@ -1,10 +1,21 @@
 from xsbs.http import urlHandler, regexUrlHandler
-from xsbs.players import all as allClients
+from xsbs.players import all as allClients, player
 import json
 
 @regexUrlHandler('/json/clients/(?P<cn>\d+)')
 def clientDetail(request, cn):
-	print cn
+	try:
+		p = player(int(cn))
+	except ValueError:
+		response = { 'error': 'Invalid cn' }
+	else:
+		response = { 'cn': p.cn,
+			'name': p.name(),
+			'team': p.team(),
+			'frags': p.frags(),
+			'deaths': p.deaths(),
+			'teamkills': p.teamkills() }
+	request.respond_with(200, 'text/html', 0, json.dumps(response))
 
 @urlHandler('/json/clients')
 def getClients(request):
