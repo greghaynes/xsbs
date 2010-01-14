eb62f6d7500bdf95fb19cf3e0ef26c9688305162
fdChasm <fd.chasm@gmail.com>
2010-01-13 23:13:47 -0800

    Cleaned up and made nospam script more efficient

--- src/pyscripts/NoSpam/nospam.py
+++ src/pyscripts/NoSpam/nospam.py
@@ -1,22 +1,21 @@
-from xsbs.commands import commandHandler, UsageError
 from xsbs.events import eventHandler
+from xsbs.timers import addTimer
 from xsbs.settings import PluginConfig
 from xsbs.ui import error, notice, warning
 from Bans.bans import ban
-from xsbs.colors import colordict, green
+from xsbs.colors import colordict
 from xsbs.players import player, all as allPlayers, playerByName, playerByIpString
-from UserPrivilege.userpriv import masterRequired
 import sbserver
 import time
 import string
 
 config = PluginConfig('nospam')
-interval = int(config.getOption('Config', 'action_interval', '5'))
-max_per_interval = int(config.getOption('Config', 'max_msgs', '6'))
-instakickmsgs = int(config.getOption('Config', 'instakickmsgs', '3'))
-ban_duration = int(config.getOption('Config', 'ban_time', '3600'))
-warnings = int(config.getOption('Config', 'warnings', '3'))
-warn_spam_message = config.getOption('Config', 'warn_spam_message', 'Warning do not spam. This server is ${red}spam intollerant!${white} ')
+interval = 			int(config.getOption('Config', 'action_interval', '5'))
+max_per_interval = 	int(config.getOption('Config', 'max_msgs', '6'))
+max_per_second = 	int(config.getOption('Config', 'max_msgs_per_sec', '3'))
+ban_duration = 		int(config.getOption('Config', 'ban_time', '3600'))
+warnings = 			int(config.getOption('Config', 'warnings', '2'))
+warn_spam_message = config.getOption('Config', 'warn_spam_message', 'Warning do not spam. This server is ${red}spam intolerant!${white} ')
 del config
 warn_spam_message = string.Template(warn_spam_message)
 
@@ -42,20 +41,23 @@ class SpammerManager:
 		self.spammerlist = {}
 		
 	def add_spamming_case(self, ip):
-		playerByIpString(ip).message(warning(warn_spam_message.substitute(colordict)))
-	
 		if ip in self.spammerlist.keys():
 			self.spammerlist[ip] += 1
 			if self.spammerlist[ip] > warnings:
 				self.dealwithspammer(ip)
 				del self.spammerlist[ip]
+			else:
+				playerByIpString(ip).message(warning(warn_spam_message.substitute(colordict)))
 		else:
 			self.spammerlist[ip] = 1
+			playerByIpString(ip).message(warning(warn_spam_message.substitute(colordict)))
 
 	def dealwithspammer(self, ip):
-		playercn = playerByIpString(ip).cn
-		ban(playercn, ban_duration, 'spamming server', -1)
-		
+		try:
+			playercn = playerByIpString(ip).cn
+			ban(playercn, ban_duration, 'spamming server', -1)
+		except ValueError:
+			print "Error while banning spamming player by IP"
 
 chatlog = ChatLog()
 spammermanager = SpammerManager()
@@ -65,32 +67,38 @@ spammermanager = SpammerManager()
 def onMessage(cn, text):
 	chatlog.add_chat(cn, text)
 	chatlog.clean_chat()
-	log = chatlog.get_log()
-	
-	cn_1secoccurance = {}
-	for timekey in log.keys():
-		if (time.time() - timekey) <= 1:
-			chatcn = log[timekey][0]
-			if not chatcn in cn_1secoccurance.keys():
-				cn_1secoccurance[chatcn] = 1
-			else:
-				cn_1secoccurance[chatcn] += 1
 
 	
-	for occur in cn_1secoccurance.keys():
-		if cn_1secoccurance[occur] > instakickmsgs:
-			spammermanager.add_spamming_case(player(occur).ipString())
-				
-	cn_occurance = {}
+def CheckForSpammers():
+	#sbserver.message("Check for spammers ran")
+	log = chatlog.get_log()
+	
+	cn_occurs_1sec = {}
+	cn_occurs_interval = {}
 	for timekey in log.keys():
 		chatcn = log[timekey][0]
-		if not chatcn in cn_occurance.keys():
-			cn_occurance[chatcn] = 1
+		if (time.time() - timekey) <= 1:
+			if not chatcn in cn_occurs_1sec.keys():
+				cn_occurs_1sec[chatcn] = 1
+			else:
+				cn_occurs_1sec[chatcn] += 1
+				
+		if not chatcn in cn_occurs_interval.keys():
+			cn_occurs_interval[chatcn] = 1
 		else:
-			cn_occurance[chatcn] += 1
+			cn_occurs_interval[chatcn] += 1
+
 	
-	
-	for occur in cn_occurance.keys():
-		if cn_occurance[occur] > max_per_interval:
+	for occur in cn_occurs_1sec.keys():
+		if cn_occurs_1sec[occur] > max_per_second:
 			spammermanager.add_spamming_case(player(occur).ipString())
+		else:
+			for occur in cn_occurs_interval.keys():
+				if cn_occurs_interval[occur] > max_per_interval:
+					spammermanager.add_spamming_case(player(occur).ipString())
+
+def checkforspammerstimer():
+	CheckForSpammers()
+	addTimer(1000, checkforspammerstimer, ())
 	
+addTimer(1000, checkforspammerstimer, ())
