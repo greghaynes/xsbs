62322e54eee8febf638722ee011720c3e6b34f2d
Gregory Haynes <greg@greghaynes.net>
2010-01-15 04:27:18 -0800

    HttpServer uses config port

--- src/pyscripts/xsbs/http/__init__.py
+++ src/pyscripts/xsbs/http/__init__.py
@@ -1,10 +1,17 @@
 from twisted.web import server, resource
 from twisted.internet import reactor
 
+from xsbs.settings import PluginConfig
+
+config = PluginConfig('httpserver')
+port = config.getOption('Config', 'port', '8081')
+port = int(port)
+del config
+
 class RootSite(resource.Resource):
 		pass
 
 site = RootSite()
 server_site = server.Site(site)
-reactor.listenTCP(8081, server_site)
+reactor.listenTCP(port, server_site)
 
