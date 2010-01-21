923ded9983e27e4af98bbd0c77ca6519af59b7e0
Gregory Haynes <greg@greghaynes.net>
2010-01-20 21:07:05 -0800

    Banner no longer uses sbserver

--- src/pyscripts/Banner/banner.py
+++ src/pyscripts/Banner/banner.py
@@ -1,26 +1,29 @@
-import sbserver
 from xsbs.settings import PluginConfig
 from xsbs.timers import addTimer
 from xsbs.colors import colordict
 from xsbs.ui import notice
+from xsbs.server import message
 import string
 
 class Banner:
-	def __init__(self, message, delay):
-		self.msg = string.Template(message).substitute(colordict)
+	def __init__(self, msg, delay):
+		self.msg = string.Template(msg).substitute(colordict)
 		self.delay = delay
 		addTimer(delay, self.sendMessage, ())
 	def sendMessage(self):
-		sbserver.message(notice(self.msg))
+		message(notice(self.msg))
 		addTimer(self.delay, self.sendMessage, ())
 
 banners = []
 
-config = PluginConfig('banner')
-default_timeout = config.getOption('Config', 'default_timeout', 180000)
-for option in config.getAllOptions('Banners'):
-	msg = option[1]
-	delay = config.getOption('Timeouts', option[0], default_timeout, False)
-	banners.append(Banner(msg, int(delay)))
-del config
+def setup():
+	config = PluginConfig('banner')
+	default_timeout = config.getOption('Config', 'default_timeout', 180000)
+	for option in config.getAllOptions('Banners'):
+		msg = option[1]
+		delay = config.getOption('Timeouts', option[0], default_timeout, False)
+		banners.append(Banner(msg, int(delay)))
+	del config
+
+setup()
 
