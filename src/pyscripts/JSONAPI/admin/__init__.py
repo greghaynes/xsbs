083a9545749855e61046a5c099d4a84a93f40e60
Gregory Haynes <greg@greghaynes.net>
2010-01-24 04:40:27 -0800

    Created /json/admin/(clients|server)/(setmap|kick)

---
+++ src/pyscripts/JSONAPI/admin/__init__.py
@@ -0,0 +1,20 @@
+from xsbs.http.jsonapi import site as jsonSite, JsonAtLeastMasterSite, response
+from xsbs.players import player
+from xsbs.game import modeNumber
+
+from JSONAPI.admin.server import setup as setupServerSite
+from JSONAPI.admin.clients import setup as setupClientsSite
+
+import sbserver
+import json
+
+class AdminSite(JsonAtLeastMasterSite):
+	pass
+
+def setup(jsonSite):
+	'''Called by JSONAPI/api.py'''
+	adminSite = AdminSite()
+	setupServerSite(adminSite)
+	setupClientsSite(adminSite)
+	jsonSite.putChild('admin', adminSite)
+
