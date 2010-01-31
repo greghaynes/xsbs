101bdb29c0d4326de5a277156cf4e96d728a157e
fdChasm <fd.chasm@gmail.com>
2010-01-31 10:13:45 -0800

    Added multikill awards and made ownage data be stored in player instances

--- src/pyscripts/GameAwards/awards.py
+++ src/pyscripts/GameAwards/awards.py
@@ -12,12 +12,35 @@ mftemp = config.getOption('Config', 'most_frags', 'Most Frags: ${orange}${name} 
 mtktemp = config.getOption('Config', 'most_teamkills', 'Most TeamKills: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
 mdtemp = config.getOption('Config', 'most_deaths', 'Most Deaths: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
 matemp = config.getOption('Config', 'most_accurate', 'Most Accurate: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
+
+newawards_enabled = config.getOption('Config', 'Multikill_awards', 'enabled') == 'enabled'
+most_doublekillstemp = config.getOption('Config', 'most_doublekills', 'Most Double Kills: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
+most_triplekillstemp = config.getOption('Config', 'most_triplekills', 'Most Triple Kills: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
+most_overkillstemp = config.getOption('Config', 'most_overkills', 'Most Overkills: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
+most_killtacularstemp = config.getOption('Config', 'most_killtaculars', 'Most Killtacular: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
+most_killotrocitiestemp = config.getOption('Config', 'most_killotrocities', 'Most Killotrocities: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
+most_killtastrophestemp = config.getOption('Config', 'most_killtastrophes', 'Most Killtastrophic: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
+most_killapocalypsestemp = config.getOption('Config', 'most_killapocalypses', 'Most Killapocalyptic: ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
+most_killionairestemp = config.getOption('Config', 'most_killionaires', 'Best Killionaire ${orange}${name} ${blue}(${green}${count}${blue})${white} ')
+
+
+
 del config
 awards_prefix = string.Template(awards_prefix).substitute(colordict)
 mftemp = string.Template(mftemp)
 mtktemp = string.Template(mtktemp)
 mdtemp = string.Template(mdtemp)
 matemp = string.Template(matemp)
+
+most_doublekillstemp = string.Template(most_doublekillstemp)
+most_triplekillstemp = string.Template(most_triplekillstemp)
+most_overkillstemp = string.Template(most_overkillstemp)
+most_killtacularstemp = string.Template(most_killtacularstemp)
+most_killotrocitiestemp = string.Template(most_killotrocitiestemp)
+most_killtastrophestemp = string.Template(most_killtastrophestemp)
+most_killapocalypsestemp = string.Template(most_killapocalypsestemp)
+most_killionairestemp = string.Template(most_killionairestemp)
+
 
 def onIntermission():
 	players = allPlayers()
@@ -30,6 +53,24 @@ def onIntermission():
 	most_accuracy = 0
 	most_accurate_cn = -1
 
+	if newawards_enabled:
+		most_doublekills = 0
+		most_doublekills_cn = -1
+		most_triplekills = 0
+		most_triplekills_cn = -1
+		most_overkills = 0
+		most_overkills_cn = -1
+		most_killtaculars = 0
+		most_killtaculars_cn = -1
+		most_killotrocities = 0
+		most_killotrocities_cn = -1
+		most_killtastrophes = 0
+		most_killtastrophes_cn = -1
+		most_killapocalypses = 0
+		most_killapocalypses_cn = -1
+		most_killionaires = 0
+		most_killionaires_cn = -1
+	
 	for p in players:
 		try:
 			frags = p.frags()
@@ -50,6 +91,78 @@ def onIntermission():
 				most_accurate_cn = p.cn
 		except ValueError:
 			continue
+			
+		if newawards_enabled:
+			try:
+				try:
+					doublekills	= p.ownagedata.multikill_counts[2]
+				except:
+					doublekills = 0
+					
+				try:
+					triplekills = p.ownagedata.multikill_counts[3]
+				except:
+					triplekills = 0
+					
+				try:
+					overkills = p.ownagedata.multikill_counts[5]
+				except:
+					overkills = 0
+					
+				try:
+					killtaculars = p.ownagedata.multikill_counts[7]
+				except:
+					killtaculars = 0
+					
+				try:
+					killotrocities	= p.ownagedata.multikill_counts[10]
+				except:
+					killotrocities = 0
+					
+				try:
+					killtastrophes 	= p.ownagedata.multikill_counts[15]
+				except:
+					killtastrophes = 0
+					
+				try:
+					killapocalypses	= p.ownagedata.multikill_counts[20]
+				except:
+					killapocalypses = 0
+					
+				try:
+					killionaires = p.ownagedata.multikill_counts[25]
+				except:
+					killionaires = 0
+				
+				
+				
+				if doublekills > most_doublekills or most_doublekills_cn == -1:
+					most_doublekills = doublekills
+					most_doublekills_cn = p.cn
+				if triplekills > most_triplekills or most_triplekills_cn == -1:
+					most_triplekills = triplekills
+					most_triplekills_cn = p.cn
+				if overkills > most_overkills or most_overkills_cn == -1:
+					most_overkills = overkills
+					most_overkills_cn = p.cn
+				if killtaculars > most_killtaculars or most_killtaculars_cn == -1:
+					most_killtaculars = killtaculars
+					most_killtaculars_cn = p.cn
+				if killotrocities > most_killotrocities or most_killotrocities_cn == -1:
+					most_killotrocities = killotrocities
+					most_killotrocities_cn = p.cn
+				if killtastrophes > most_killtastrophes or most_killtastrophes_cn == -1:
+					most_killtastrophes = killtastrophes
+					most_killtastrophes_cn = p.cn
+				if killapocalypses > most_killapocalypses or most_killapocalypses_cn == -1:
+					most_killapocalypses = killapocalypses
+					most_killapocalypses_cn = p.cn
+				if killionaires > most_killionaires or most_killionaires_cn == -1:
+					most_killionaires = killionaires
+					most_killionaires_cn = p.cn
+			except (KeyError, ValueError):
+				continue
+	
 	msg = ''
 	if most_frags > 0:
 		msg += mftemp.substitute(colordict, name=player(most_frags_cn).name(), count=most_frags)
@@ -62,6 +175,34 @@ def onIntermission():
 		msg += ' '
 	if most_accuracy > 0:
 		msg += matemp.substitute(colordict, name=player(most_accurate_cn).name(), count=most_accuracy)
+
+		
+	if newawards_enabled:
+		if most_doublekills > 0:
+			msg += most_doublekillstemp.substitute(colordict, name=player(most_doublekills_cn).name(), count=most_doublekills)
+			msg += ' '
+		if most_triplekills > 0:
+			msg += most_triplekillstemp.substitute(colordict, name=player(most_triplekills_cn).name(), count=most_triplekills)
+			msg += ' '
+		if most_overkills > 0:
+			msg += most_overkillstemp.substitute(colordict, name=player(most_overkills_cn).name(), count=most_overkills)
+			msg += ' '
+		if most_killtaculars > 0:
+			msg += most_killtacularstemp.substitute(colordict, name=player(most_killtaculars_cn).name(), count=most_killtaculars)
+			msg += ' '
+		if most_killotrocities > 0:
+			msg += most_killotrocitiestemp.substitute(colordict, name=player(most_killotrocities_cn).name(), count=most_killotrocities)
+			msg += ' '
+		if most_killtastrophes > 0:
+			msg += most_killtastrophestemp.substitute(colordict, name=player(most_killtastrophes_cn).name(), count=most_killtastrophes)
+			msg += ' '
+		if most_killapocalypses > 0:
+			msg += most_killapocalypsestemp.substitute(colordict, name=player(most_killapocalypses_cn).name(), count=most_killapocalypses)
+			msg += ' '
+		if most_killionaires > 0:
+			msg += most_killionairestemp.substitute(colordict, name=player(most_killionaires_cn).name(), count=most_killionaires)
+			msg += ' '
+	
 	if msg != '':
 		msg = awards_prefix + msg
 		serverMessage(msg)
--- src/pyscripts/OwnageMessages/ownage.py
+++ src/pyscripts/OwnageMessages/ownage.py
@@ -20,7 +20,8 @@ spreemessages = {
 	15: string.Template(green('$name') + ' is ' + orange('GODLIKE!')) 
 	}
 endmsg = string.Template(orange('$victimname') + '\'s killing spree ended by ' + green('$killername'))
-	
+suicideendmsg = string.Template(orange('$victimname') + 'has ended his own killing spree!')
+
 neomessages = { 
 	2: string.Template(orange('DOUBLE KILL!')),
 	3: string.Template(orange('TRIPLE KILL!')),
@@ -37,108 +38,147 @@ dommessages = {
 	25: string.Template(green('$killername') + ' is ' + orange('BRUTILIZING ') + green('$victimname'))
 	}
 
-class KillLog:
-	def __init__(self):
-		self.log = {}
-	
-	def add_kill(self, killercn, victimcn):
-		if killercn in self.log.keys():
-		
-			killssincedeath = self.log[killercn][4] + 1
-			self.log[killercn][5][0] = False
-		
-		
-			if (time.time() - self.log[killercn][0]) < interval:
-				wasownage = self.log[killercn][2] + 1
-				self.log[killercn][5][1] = False
-			else:
-				wasownage = 1
-				
-			if victimcn == self.log[killercn][1]:
-				wasdomination = self.log[killercn][3] + 1
-				self.log[killercn][5][2] = False
-			else:
-				wasdomination = 1
-			
+class ownageData:
+	def __init__(self, cn):
+		self.playercn = cn
+		self.last_kill_time = 0
+		self.last_victim = -1
+		self.current_victim = -2
+		self.kills_since_death = 0
+		self.ownage_count = 0
+		self.last_ownage_count = 0
+		self.domination_count = 0
 
-		else:
-			wasownage = 1
-			wasdomination = 1
-			killssincedeath = 1
-				
-		self.log[killercn] = (time.time(), victimcn, wasownage, wasdomination, killssincedeath, [False, False, False])
+		self.multikill_counts = {}
+		for key in neomessages.keys():
+			self.multikill_counts[key] = 0
 		
-		if victimcn in self.log.keys():
-			if self.log[victimcn][4] >= 5:
-				sbserver.message(info(endmsg.substitute(victimname=sbserver.playerName(victimcn), killername=sbserver.playerName(killercn))))
-			
-		self.clearstats(victimcn)
-
-	def set_said(self, cn, msg):
-		if msg == 1:
-			self.log[cn][5][0] = True
-		elif msg == 2:
-			self.log[cn][5][1] = True
-		elif msg == 3:
-			self.log[cn][5][2] = True
+	#######public#######	
 		
-	def clearstats(self, cn):
-		self.log[cn] = [0, -1, 0, 0, 0, [False, False, False]]
-	
-	def get_log(self):
-		return self.log
-
-killlog = KillLog()
-
-def CheckForOwnage():
-	log = killlog.get_log()
-	
-	for killercn in log.keys():
-		#impliment traditional killing spree messages
+	def commit_kill(self, victimcn):
+		self.kills_since_death += 1
+		self.current_victim = victimcn
+		self.current_kill_time = time.time()
+		
 		if spreemessagesenable:
-			if log[killercn][4] > 1 and (not log[killercn][5][0]):
-				try:
-					spreemsg = spreemessages[log[killercn][4]]
-					sbserver.message(info(spreemsg.substitute(name=sbserver.playerName(killercn))))
-					log[killercn][5][0] = True
-				except KeyError:
-					pass
-		#impliment halo-like multi-kill messages
+			self.check_sprees()
 		if neomessagesenable:
-			if log[killercn][2] > 1 and (not log[killercn][5][1]):
-				try:
-					neomessage = neomessages[log[killercn][2]]
-					playerinst = player(killercn)
-					playerinst.message(info(neomessage.substitute()))
-					log[killercn][5][1] = True
-				except KeyError:
-					pass
-		#impliment domination messsages.
+			self.check_ownage()
 		if dommessagesenable:
-			if log[killercn][3] > 1 and (not log[killercn][5][2]):
+			self.check_domination()
+
+		self.last_kill_time = self.current_kill_time	
+		self.last_victim = self.current_victim
+
+	
+	def commit_death(self, killercn=-1):
+		if killercn != -1:
+			self.check_if_ending_spree(killercn)
+	
+		self.kills_since_death = 0
+		self.last_kill_time = 0
+		self.current_kill_time	= 10
+		self.last_victim = -1
+		self.current_victim = -2
+		self.kills_since_death = 0
+		self.ownage_count = 0
+		self.last_ownage_count = 0
+		self.domination_count = 0
+		
+
+	######private#######
+	#implement traditional killing spree messages
+	def check_sprees(self):
+		if self.kills_since_death in spreemessages.keys():
+			sbserver.message(info(spreemessages[self.kills_since_death].substitute(name=player(self.playercn).name())))
+			
+	def check_if_ending_spree(self, killercn):
+		if self.kills_since_death >= 5:
+			if killercn == -2:
+				sbserver.message(info(suicideendmsg.substitute(victimname=player(self.playercn).name())))
+			else:
+				sbserver.message(info(endmsg.substitute(victimname=player(self.playercn).name(), killername=player(killercn).name())))
+			
+
+	#implement halo-like multi-kill messages
+	def check_ownage(self):
+		if (self.current_kill_time - self.last_kill_time) < interval:
+			self.ownage_count += 1
+			
+			#check whether this level of multikill warrants a message
+			if self.ownage_count in neomessages.keys():
 				try:
-					dommessage = dommessages[log[killercn][3]]
-					sbserver.message(info(dommessage.substitute(killername=sbserver.playerName(killercn), victimname=sbserver.playerName(log[killercn][1]))))
-					log[killercn][5][2] = True
-				except KeyError:
+					player(self.playercn).message(info(neomessages[self.ownage_count].substitute()))
+				except ValueError:
 					pass
+				self.last_ownage_count = self.ownage_count
+		else:
+			#that multikill session ended so the multikill counter should be incremented
+			if self.last_ownage_count != 0:
+				self.multikill_counts[self.last_ownage_count] += 1
+			self.ownage_count = 1
+			
+
+	#implement domination messsages
+	def check_domination(self):
+		if self.last_victim == self.current_victim:
+			self.domination_count += 1
+		else:
+			self.domination_count = 0
+		if self.domination_count in dommessages.keys():
+			sbserver.message(info(dommessages[self.domination_count].substitute(killername=player(self.playercn).name(), victimname=player(self.last_victim).name())))
+
+class ownageManager:
+	def __init__(self):
+		players = allPlayers()
+		for p in players:
+			p.ownagedata = ownageData(p.cn)
+		
+	def connect(self, cn):
+		player(cn).ownagedata = ownageData(cn)
+		
+	def add_kill(self, killercn, victimcn):
+		#updates the ownage datatype for the killer and victim
+		killerplayer = player(killercn)
+		victimplayer = player(victimcn)
+
+		killerplayer.ownagedata.commit_kill(victimcn)
+		victimplayer.ownagedata.commit_death(killercn)
+
+		
+	def suicide(self, cn):
+			player(cn).ownagedata.commit_death(-2)
+			
+	def teamkill(self, killercn, victimcn):
+		player(victimcn).ownagedata.commit_death(killercn)
+		player(killercn).ownagedata.commit_death()
 	
+
+		
+ownagemanager = ownageManager()
+
 	
+@eventHandler('map_changed')
+def onMapCh(mapname, mapmode):
+	ownagemanager = ownageManager()
 	
 @eventHandler('player_frag')
 def onFrag(killercn, victimcn):
-	killlog.add_kill(killercn, victimcn)
-	CheckForOwnage()
+	ownagemanager.add_kill(killercn, victimcn)
 	
 @eventHandler('player_teamkill')
 def onTK(killercn, victimcn):
-	killlog.clearstats(killercn)
+	ownagemanager.teamkill(killercn, victimcn)
 	
-@eventHandler('player_disconnect')
+@eventHandler('player_connect')
 def onDisconnect(cn):
-	killlog.clearstats(cn)
+	ownagemanager.connect(cn)
+	
+@eventHandler('game_bot_added')
+def onDisconnect(cn):
+	ownagemanager.connect(cn)
 	
 @eventHandler('player_suicide')
 def onSuicide(cn):
-	killlog.clearstats(cn)
+	ownagemanager.suicide(cn)
 	
