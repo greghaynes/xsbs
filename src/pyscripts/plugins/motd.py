2e1f6fc2ef999cc3ee8d2bdf3531ae0e36666aed
darkf <lw9k123@gmail.com>
2010-03-04 00:40:37 -0600

    Made motd use loadPluginConfig and add ${name}

--- src/pyscripts/plugins/motd.py
+++ src/pyscripts/plugins/motd.py
@@ -1,15 +1,22 @@
-import sbserver
-from xsbs.settings import PluginConfig
+from xsbs.settings import loadPluginConfig
 from xsbs.colors import colordict
 from xsbs.events import eventHandler
+from xsbs.players import player
 import string
+
+#config = PluginConfig('motd')
+#motdstring = config.getOption('Config', 'template', '${orange}Welcome to a ${red}XSBS ${orange}server, ${green}${name}${white}!')
+
+config = {
+	'Templates': {
+		'motd': '${orange}Welcome to a ${red}XSBS ${orange}server, ${green}${name}${white}!'
+		}
+	}
+
+loadPluginConfig(config, 'motd')
+motdstring = string.Template(config['Templates']['motd'])
 
 @eventHandler('player_connect_delayed')
 def greet(cn):
-	sbserver.playerMessage(cn, motdstring)
-
-config = PluginConfig('motd')
-motdstring = config.getOption('Config', 'template', '${orange}Welcome to a ${red}XSBS ${orange}server')
-del config
-
-motdstring = string.Template(motdstring).substitute(colordict)
+	p = player(cn)
+	p.message(motdstring.substitute(colordict, name=p.name()))
