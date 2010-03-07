e0c7d2caaa05ea9587b35b4e00b5e2de0d7c87b2
Gregory Haynes <greg@greghaynes.net>
2010-03-07 03:18:00 -0800

    Added skeleton json config site

---
+++ src/pyscripts/plugins/JSONAPI/admin/config.py
@@ -0,0 +1,37 @@
+from xsbs.http.jsonapi import JsonAdminSite
+from xsbs.settings import pluginNames, pluginSections, sectionOptions, setOption
+
+try:
+	import json
+except ImportError:
+	import simplejson as json
+
+class ConfigPluginSite(JsonAdminSite):
+	def __init__(self, plugin_name):
+		self.plugin_name = plugin_name
+	def render_admin_JSON(self, request):
+		return json.dumps({
+			'plugin': self.plugin_name,
+			'sections': pluginSections(self.plugin_name)
+			})
+
+class ConfigSectionSite(JsonAdminSite):
+	def __init__(self, plugin_name, section_name):
+		self.plugin_name = plugin_name
+		self.section_name = section_name
+	def render_admin_JSON(self, request):
+		return json.dumps({
+			'plugin': self.plugin_name,
+			'section': self.section_name,
+			'options': sectionOptions(self.plugin_name, self.section_name)
+			})
+
+def init(site):
+	configSite = JsonAdminSite()
+	for name in pluginNames():
+		pluginConfigSite = ConfigPluginSite(name)
+		for section in pluginSections(name):
+			pluginConfigSite.putChild(section, ConfigSectionSite(name, section))
+		configSite.putChild(name, pluginConfigSite)
+	site.putChild('config', configSite)
+
