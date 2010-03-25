035561bc0c26cfa7e4b297ad73c41432f692a277
darkf <lw9k123@gmail.com>
2010-03-25 01:53:45 -0500

    Fix user account registration and login

--- src/pyscripts/xsbs/users/__init__.py
+++ src/pyscripts/xsbs/users/__init__.py
@@ -72,7 +72,7 @@ def isValidEmail(email):
 	return False
 
 @commandHandler('register')
-def onRegisterCommand(cn, args):
+def onRegisterCommand(p, args):
 	'''@description Register account with server
 	   @usage email password
 	   @public'''
@@ -86,14 +86,14 @@ def onRegisterCommand(cn, args):
 			raise ArgumentValueError('Invalid email address')
 		user = User(args[0], args[1])
 		session.commit()
-		sbserver.playerMessage(cn, info('Account created'))
+		p.message(info('Account created'))
 		return
 	except MultipleResultsFound:
 		pass
 	raise StateError('An account with that email already exists')
 
 @commandHandler('login')
-def onLoginCommand(cn, args):
+def onLoginCommand(p, args):
 	'''@description Login to server account
 	   @usage email password
 	   @public'''
@@ -102,63 +102,63 @@ def onLoginCommand(cn, args):
 		raise UsageError()
 	user = userAuth(args[0], args[1])
 	if user:
-		login(cn, user)
+		login(p.cn, user)
 	else:
-		sbserver.playerMessage(cn, error('Invalid login.'))
+		p.message(error('Invalid login.'))
 
 @commandHandler('linkname')
-def onLinkName(cn, args):
+def onLinkName(p, args):
 	'''@description Link name to server account, and reserve name.
 	   @usage
 	   @public'''
 	if args != '':
 		raise UsageError()
-	if not isLoggedIn(cn):
+	if not isLoggedIn(p.cn):
 		raise StateError('You must be logged in to link a name to your account')
-	if sbserver.playerName(cn) in blocked_names:
-		raise StateError('You can not reserve this name')
+	if p.name() in blocked_names:
+		raise StateError('You cannot reserve this name')
 	try:
-		NickAccount.query.filter(NickAccount.nick==sbserver.playerName(cn)).one()
+		NickAccount.query.filter(NickAccount.nick==p.name()).one()
 	except NoResultFound:
-		user = loggedInAs(cn)
-		nickacct = NickAccount(sbserver.playerName(cn), user)
+		user = loggedInAs(p.cn)
+		nickacct = NickAccount(p.name(), user)
 		session.commit()
-		sbserver.playerMessage(cn, info('\"%s\" is now linked to your account.' % (sbserver.playerName(cn))))
-		sbserver.playerMessage(cn, info('You may now login with /setmaster password'))
+		p.message(info('\"%s\" is now linked to your account.' % p.name()))
+		p.message(info('You may now login with /setmaster password'))
 		return
 	except MultipleResultsFound:
 		pass
 	raise StateError('Your name is already linked to an account')
 
 @commandHandler('newuser')
-def onNewuserCommand(cn, args):
+def onNewuserCommand(p, args):
 	'''@description Register account with server
 	   @usage email password
 	   @public'''
-	onRegisterCommand(cn, args)
-	onLoginCommand(cn, args)
-	onLinkName(cn, '')
+	onRegisterCommand(p.cn, args)
+	onLoginCommand(p.cn, args)
+	onLinkName(p.cn, '')
 
 @commandHandler('changepass')
-def onChangepass(cn, args):
+def onChangepass(p, args):
 	'''@description Change your password
 	   @usage old_password new_password
 	   @public'''
 	args = args.split(' ')
 	if len(args) != 2:
 		raise UsageError()
-	if not isLoggedIn(cn):
+	if not isLoggedIn(p.cn):
 		raise StateError('You must be logged in to change your password')
 	try:
-		User.query.filter(User.id==loggedInAs(cn).id).filter(User.password==args[0]).one()
+		User.query.filter(User.id==loggedInAs(p.cn).id).filter(User.password==args[0]).one()
 	except NoResultFound:
 		raise StateError('Incorrect password.')
 	except MultipleResultsFound:
 		pass
 	else:
-		if not isValidPassword(args[1], cn):
+		if not isValidPassword(args[1], p.cn):
 			raise ArgumentValueError('Invalid password')
-		User.query.filter(User.id==loggedInAs(cn).id).update({ 'password': args[1] })
+		User.query.filter(User.id==loggedInAs(p.cn).id).update({ 'password': args[1] })
 		session.commit()
 		return
 
