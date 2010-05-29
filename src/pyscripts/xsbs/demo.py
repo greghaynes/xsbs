e4e7adb5f4b263a6bbe3adccfc9f31046573e9be
Chasm <fd.chasm@gmail.com>
2010-05-28 20:23:18 -0700

    persistdemo function actually works now and the config now uses the database

--- src/pyscripts/xsbs/demo.py
+++ src/pyscripts/xsbs/demo.py
@@ -2,44 +2,44 @@ import sbserver
 from xsbs.commands import commandHandler, UsageError
 from xsbs.events import eventHandler
 from xsbs.ui import info, notice
-from xsbs.settings import PluginConfig
+from xsbs.settings import loadPluginConfig
 from xsbs.colors import colordict
 from xsbs.players import player, masterRequired, adminRequired, isUserAdmin, isUserAtLeastMaster
 from xsbs.ui import insufficientPermissions
 import string
 import logging
 
-config = PluginConfig('demorecord')
-action_temp = config.getOption('Config', 'record_next_message', 'Demo recording is ${action} for next match (by ${orange}${user}${white}${white}${white}${white})')
-persistent_recording = config.getOption('Config', 'persistent_recording', 'no') == 'yes'
-required_permissions = config.getOption('Config', 'required_permissions', 'master')
-del config
-action_temp = string.Template(action_temp)
-if required_permissions == 'user':
-	required_permissions = 0
-elif required_permissions == 'master':
-	required_permissions = 1
-elif required_permissions == 'admin':
-	required_permissions = 2
-else:
-	logging.error('invalid required_permissions')
-	required_permissions = 1
+config = {
+	'Main': {
+		'persistent_recording': 'no',
+		'required_permissions': 1
+		},
+	'Templates': {
+		'record_next_message': 'Demo recording is ${action} for next match (by ${orange}${user}${white}${white}${white}${white})'
+		}
+	}
+	
+loadPluginConfig(config, 'Demos')
 
-def permissions_ok(cn):
+persistent_recording = config['Main']['persistent_recording'] == 'yes'
+required_permissions = config['Main']['required_permissions']
+
+action_temp = string.Template(config['Templates']['record_next_message'])
+
+def permissions_ok(p):
 	if required_permissions == 0:
 		return True
-	p = player(cn)
 	if required_permissions == 1:
 		return p.isAtLeastMaster()
 	if required_permissions == 2:
 		return p.isAdmin()
-	logging.error('required_permissions not an int!')
+	logging.error('required_permissions not a valid int!')
 	return False
 	
-
+	
 @eventHandler('player_record_demo')
 def playerRecordNextMatch(p, val):
-	if permissions_ok(p.cn):
+	if permissions_ok(p):
 		if val == sbserver.nextMatchRecorded():
 			return
 		if val:
@@ -51,24 +51,28 @@ def playerRecordNextMatch(p, val):
 	else:
 		insufficientPermissions(p.cn)
 
+
 @eventHandler('map_changed')
 def persistRecordNextMatch(themap, themode):
-	sbserver.setRecordNextMatch(persistent_recording)
+	global persistent_recording
+	if persistent_recording:
+		sbserver.setRecordNextMatch(True)
+
 
 @commandHandler('persistdemo')
 @adminRequired
 def setPersistantDemoRecord(p, args):
 	'''@description Enable/disable persistant demo recording
 	   @usage enable/disable'''
+	global persistent_recording
 	if args == 'enable':
 		p.message(info('Enabling persistant demo recording'))
 		persistent_recording = True
 		sbserver.setRecordNextMatch(persistent_recording)
-		
 	elif args == 'disable':
 		p.message(info('Disabling persistant demo recording'))
 		persistent_recording = False
 		sbserver.setRecordNextMatch(persistent_recording)
 	else:
 		raise UsageError()
-
+		
