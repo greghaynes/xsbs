8f3b70cb34585192da2b8f2444b5a118d587b1e7
Gregory Haynes <greg@greghaynes.net>
2010-07-20 14:35:08 -0700

    Added player_message back

--- src/fpsgame/server.cpp
+++ src/fpsgame/server.cpp
@@ -1958,11 +1958,15 @@ namespace server
 
             case N_TEXT:
             {
-                QUEUE_AI;
-                QUEUE_MSG;
                 getstring(text, p);
                 filtertext(text, text);
-                QUEUE_STR(text);
+                if(SbPy::triggerPolicyEventIntString("allow_message", ci->clientnum, text))
+                {
+                    SbPy::triggerEventIntString("player_message", ci->clientnum, text);
+                    QUEUE_AI;
+                    QUEUE_MSG;
+                    QUEUE_STR(text);
+                }
                 break;
             }
 
