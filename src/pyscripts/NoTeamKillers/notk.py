58b0a286a859dca6889781754dfac995be4c4d15
lfeagan <lfeagan@localhost.(none)>
2010-07-22 00:39:06 -0500

    adds the #disabletklimit #enabletklimit and #cleartks commands to the NoTeamKillers module

--- src/pyscripts/NoTeamKillers/notk.py
+++ src/pyscripts/NoTeamKillers/notk.py
@@ -1,9 +1,12 @@
 from Bans.bans import ban
 from xsbs.settings import PluginConfig
 from xsbs.events import registerServerEventHandler
-from xsbs.players import player
-from xsbs.ui import warning
+from xsbs.players import masterRequired, player
+from xsbs.ui import notice, warning, error
 from xsbs.colors import colordict
+from xsbs.commands import commandHandler, UsageError, ArgumentValueError
+from xsbs.server import message as serverMessage
+import sbserver
 import string
 
 config = PluginConfig('notk')
@@ -13,15 +16,44 @@ warn_tk_limit = config.getOption('Config', 'warn_tk_limit', 'no') == 'yes'
 warn_tk_message = config.getOption('Config', 'warn_tk_message', 'This server does not allow more than ${red}${limit}${white} teamkills per game')
 del config
 warn_tk_message = string.Template(warn_tk_message)
+enableTeamKillLimit = True
 
 def onTeamkill(cn, tcn):
-	try:
-		if player(cn).teamkills() >= limit:
-			ban(cn, duration, 'killing teammates', -1)
-		elif warn_tk_limit and player(cn).teamkills() == 1:	
-			player(cn).message(warning(warn_tk_message.substitute(colordict, limit=limit)))
-	except KeyError:
-		pass
+        global enableTeamKillLimit
+        if enableTeamKillLimit == True:
+                sbserver.message(notice('Team-kill limit is enabled'))
+                if player(cn).teamkills() >= limit:
+                        ban(cn, duration, 'killing teammates', -1)
+                elif warn_tk_limit and player(cn).teamkills() == 1:
+                        player(cn).message(warning(warn_tk_message.substitute(colordict, limit=limit)))
 
 registerServerEventHandler('player_teamkill', onTeamkill)
+
+@commandHandler('disabletklimit')
+@masterRequired
+def onDisableTeamKillLimitCommand(cn, args):
+	'''@description Disable the team-kill limit
+	   @usage '''
+	global enableTeamKillLimit
+	enableTeamKillLimit = False
+	sbserver.message(notice('Team-kill limit disabled'))
+
+@commandHandler('enabletklimit')
+@masterRequired
+def onEnableTeamKillLimitCommand(cn, args):
+	'''@description Enable the team-kill limit
+	   @usage '''
+	global enableTeamKillLimit
+	enableTeamKillLimit = True
+	sbserver.message(notice('Team-kill limit enabled'))
+
+@commandHandler('cleartks')
+@masterRequired
+def onClearTksCommand(cn, args):
+	'''@description Clear (reset) team-kill counter for a player
+	   @usage <cn>'''
+	tcn = int(args)
+	t = player(tcn)
+	serverMessage(info(kick_message.substitute(colordict, name=p.name())))
+	t.kick()
 
